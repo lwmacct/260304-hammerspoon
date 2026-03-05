1c858744916e82984c96135a81a78b6a335a2f6c
lwmacct <lwmacct@icloud.com>
2026-03-05 15:52:56 +0800

    chore 2026-03-05 15:52:56 CST

--- modules/scimkiller.lua
+++ modules/scimkiller.lua
@@ -1,13 +1,18 @@
 -- ~/.hammerspoon/init.lua
 local targets = { "SCIM", "SCIM_Extension", "TCIM", "TCIM_Extension" }
 
+local function restartDock()
+  hs.task.new("/usr/bin/killall", nil, { "Dock" }):start()
+end
+
 local function killIME()
   for _, name in ipairs(targets) do
     hs.task.new("/usr/bin/pkill", nil, { "-9", name }):start()
   end
+  restartDock()
   hs.notify.new({
     title = "SCIMKiller",
-    informativeText = "Killed SCIM/TCIM processes",
+    informativeText = "Killed SCIM/TCIM processes and restarted Dock",
   }):send()
 end
 
@@ -15,8 +20,7 @@ end
 local mb = hs.menubar.new()
 mb:setTitle("IME")
 mb:setClickCallback(killIME)
-mb:setTooltip("Kill SCIM/TCIM and extensions")
+mb:setTooltip("Kill SCIM/TCIM and extensions, then restart Dock")
 
 -- 快捷键：Ctrl + Alt + Cmd + K
 hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "K", killIME)
-
